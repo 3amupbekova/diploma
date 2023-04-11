36b2457e110aa6f02418be3ca9502c29c8affdef
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-04-11 15:36:33 +0600

    Add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {collection,getFirestore}from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db =getFirestore(app);
+
+export const categoryCollection = collection(db, 'categories');
