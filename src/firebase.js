8ad3c1c361cb0d1481e320b097e6c3139906cdf4
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-04-11 16:20:01 +0600

    Added firebase

--- src/firebase.js
+++ src/firebase.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestorу(app);
 
 export const categoryCollection = collection(db, "categories");
