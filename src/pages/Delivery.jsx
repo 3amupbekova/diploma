b057970f5f5d2054ddd1014c551dce90e9dfa47e
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-04-18 01:09:38 +0600

    Added information about the delivery of flowers

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -2,8 +2,13 @@ export default function Delivery(){
   return(
     <div className="Delivery">
       <h1>Free delivery</h1>
-      <p>You say or write your adress, and we fast delivery your home.</p>
+      <p>
+      We have delivery around the clock. We will deliver quickly and accurately, and most importantly, qualitatively. For delivery, you can contact the specified contacts on our page. Also, upon delivery from us, you will receive a bonus postcard and a chocolate bar as a gift. 
+      </p>
 
+      <p>
+      Thanks for choosing us!
+      </p>
 
     </div>
   )
