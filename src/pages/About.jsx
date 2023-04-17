899c550b30d3504551c436eef2301d7cf8130d58
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-04-18 01:02:15 +0600

    Added new information about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,32 @@
-export default function About(){
-  return(
+export default function About() {
+  return (
     <div className="About">
-      <p>We are flowers shop . We are have many different flowers. You can call our and talk what are you like ,we find and delivery.</p>
+      <p>About Us</p>
+      <p>
+        We will deliver flowers according to your wishes! We are glad to welcome
+        you in our online flower shop "Million petals"
+      </p>
+      <p>
+        We give fragrant-bouquet happiness, admire the radiance of the enchanted
+        eyes of our customers and help to bring the most daring, brightest and
+        most promising dreams into reality. We are a rapidly growing company.
+      </p>
+      <p>
+        Our team consists of young and bright employees who will never leave you
+        without attention, we will always help and advise you on any questions
+        of interest. Order flowers for an anniversary, wedding, graduation,
+        corporate holiday or any important event for you - we will realize all
+        your wildest fantasies! We understand flower issues professionally - we
+        know for sure what you can give a girl, woman, child, man of any age and
+        social status.
+      </p>
+      <p>
+        We are ready to bring flowers at any time of the day or night: our
+        company works around the clock. Delivery terms are flexible and most
+        convenient for you. We will deliver everything you need in the best
+        possible way and right on time.
+      </p>
+      <p>We will be glad to see you among our regular customers!</p>
     </div>
   );
 }
