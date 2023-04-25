e223ea4a544243ba07ced108e1acb7d028b4fa5a
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-04-25 15:32:17 +0600

    Added new image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ export default function Home(){
       <h1>Welcome to my flowers shop!</h1>
       <p> We will deliver flowers according to your wishes! We are glad to welcome
         you in our online flower shop "Million petals" </p>
+        <img src="https://avatars.dzeninfra.ru/get-zen_doc/147743/pub_5b2cf84247d07400a83f6732_5b2cfae2c8dfbc00a8552ee0/scale_1200" alt="" />
       
 
 
