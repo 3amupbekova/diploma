39ea72b9f77369b39db2d12b8e8f74724ce369dc
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-02 12:44:48 +0600

    changed the picture

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -12,7 +12,7 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <Link to="/cart">
-        <button>Cart</button>({total})
+        <img src="https://i.pinimg.com/564x/9a/0f/97/9a0f97efb79c5c2ca85a6c5e0e7f9b28.jpg" alt="" />({total})
       </Link>
     </div>
   );
