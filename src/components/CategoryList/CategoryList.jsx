fb6cbd65e6cdca2efe5908c05ae08509d7fd4e56
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-02 14:21:34 +0600

    Added new components Add Category

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { getDocs } from "firebase/firestore";
 import { categoryCollection } from "../../firebase";
 import { AppContext } from "../../App";
+import AddCategory from "../AddCategory/AddCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
@@ -17,6 +18,7 @@ export default function CategoryList() {
   return (
     <div className="CategoryList">
       <ul>{output}</ul>
+      <AddCategory/>
     </div>
   );
 }
