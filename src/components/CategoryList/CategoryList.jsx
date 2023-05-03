9637adf399a42272072e4e0bdfc3d7552ccaec59
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-03 14:21:15 +0600

    import delete category

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,10 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext} from "react";
 import "./CategoryList.css";
 import { NavLink } from "react-router-dom";
-import { getDocs } from "firebase/firestore";
-import { categoryCollection } from "../../firebase";
 import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../../DeleteCategory/DeleteCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
@@ -12,9 +11,10 @@ export default function CategoryList() {
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+      <DeleteCategory category={category}/>
     </li>
   ));
-
+  
   return (
     <div className="CategoryList">
       <ul>{output}</ul>
