1ffceec0a00e15aeff71a35694604534a1a9a147
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-03 14:20:09 +0600

    import doc

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,11 @@ import Delivery from "./pages/Delivery";
 import Contacts from "./pages/Contacts";
 import Category from "./pages/Category";
 import { createContext, useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore";
 import {
-  categoryCollection,
   onAuthChange,
-  productCollection,
-  orderCollection
+  onCategoriesLoad,
+  onProductsLoad,
+  onOrdersLoad
 } from "./firebase";
 import Payment from "./pages/Payment";
 import Reviews from "./pages/Reviews";
@@ -38,78 +37,25 @@ export default function App() {
   const [products, setProducts] = useState([]);
   const [orders, setOrders] = useState([]);
 
-  // состояние которое хранит информацию пользователя
-  const [user, setUser] = useState(null);
-
-  // корзина
   const [cart, setCart] = useState(() => {
-    // восстановить содержимое корзинки из памяти браузера.
     return JSON.parse(localStorage.getItem("cart")) || {};
   });
 
-  // выполнить эту функцию только когда содержимое корзинки меняется
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
-    // сохранить содержимое корзинки в памяти браузера
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  // выполнить эту функцию только один раз
   useEffect(() => {
-    // получить категории из списка категорий
-    getDocs(categoryCollection).then((snapshot) => {
-      // категории будут храниться в snapshot.docs
-
-      // создать массив для категорий
-      const newCategories = [];
-      // заполнить массив данными из списка категорий
-      snapshot.docs.forEach((doc) => {
-        // doc = категория
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-      // задать новый массив как состояние комапо
-      setCategories(newCategories);
-    });
-
-    // получить продукты из списка продуктов
-    getDocs(orderCollection).then((snapshot) => {
-      // продукты будут храниться в snapshot.docs
-
-      // создать массив для продуктов
-      const newOrders = [];
-      // заполнить массив данными из списка продвук
-      snapshot.docs.forEach((doc) => {
-        // doc = продукт
-        const order = doc.data();
-        order.id = doc.id;
-
-        newOrders.push(order);
-      });
-      // задать новый массив как состояние комапо
-      setOrders(newOrders);
-    });
-
-    // получить продукты из списка продуктов
-    getDocs(productCollection).then((snapshot) => {
-      // продукты будут храниться в snapshot.docs
-
-      // создать массив для продуктов
-      const newProducts = [];
-      // заполнить массив данными из списка продвук
-      snapshot.docs.forEach((doc) => {
-        // doc = продукт
-        const product = doc.data();
-        product.id = doc.id;
-
-        newProducts.push(product);
-      });
-      // задать новый массив как состояние комапо
-      setProducts(newProducts);
-    });
+    onCategoriesLoad(setCategories);
+    onProductsLoad(setProducts);
+    onOrdersLoad(setOrders);
 
     onAuthChange((user) => {
+      if (user) {
+        user.isAdmin = user.email === "elana.zamirbekovaa@gmail.com";
+      }
       setUser(user);
     });
   }, []);
