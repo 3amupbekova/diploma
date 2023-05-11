6e03dff95fe75f5b14b45d9e48c80ddaaf47fe54
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-11 23:32:18 +0600

    Added new background for my page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,6 +9,41 @@ export default function Contacts() {
          </p>
       <h2>Email</h2>
       <p>elana.zamirbekova@gmail.com</p>
+
+
+      
+
+      <div class="card">
+  
+  <div class="card__top">
+    <a href="#" class="card__image">
+      <img
+        src="https://www.shkolazhizni.ru/img/content/i117/117543_or.jpg"
+        alt="Rouse"
+      />
+    </a>
+   
+
+    <div class="card__label">-10%</div>
+  </div>
+ 
+  <div class="card__bottom">
+    <div class="card__prices">
+      <div class="card__price card__price--discount">135 000</div>
+      <div class="card__price card__price--common">150 000</div>
     </div>
+    <a href="#" class="card__title">
+      Свяжись с нами по этим номерам 
+      и получи скидку
+    </a>
+    <button class="card__add">Связаться</button>
+  </div>
+</div>
+
+
+
+
+      </div>
+    // </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ export default function Home(){
       <p> We will deliver flowers according to your wishes! We are glad to welcome
         you in our online flower shop "Million petals" </p>
         <img src="https://avatars.dzeninfra.ru/get-zen_doc/147743/pub_5b2cf84247d07400a83f6732_5b2cfae2c8dfbc00a8552ee0/scale_1200" alt="" />
+
+
+        
     </div>
   )
 }
