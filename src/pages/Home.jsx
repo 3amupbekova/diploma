7010095bc1f4c106a7acd848bc16087ff4ff5c99
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-12 14:57:06 +0600

    Newstyle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,11 @@ export default function Home(){
   return(
     <div className="Home">
       <h1>Welcome to my flowers shop</h1>
-      <p> We will deliver flowers according to your wishes! We are glad to welcome
-        you in our online flower shop "Million petals" </p>
-        <img src="https://avatars.dzeninfra.ru/get-zen_doc/147743/pub_5b2cf84247d07400a83f6732_5b2cfae2c8dfbc00a8552ee0/scale_1200" alt="" />
+      <button> We will deliver flowers according to your wishes! 
+        <br></br>We are glad to welcome
+        you in our online flower shop "Million petals" </button>
+        <img src="https://avatars.mds.yandex.net/i?id=c2052abe8ac4753c79619dc160f7b29b944fe170-8159483-images-thumbs&n=13" alt="" />
 
-
-        
     </div>
   )
 }
