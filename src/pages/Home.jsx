2833d69f29d78677142019f58bd3938ec5b24501
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-12 16:23:46 +0600

    Delete button and img in component Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ export default function Home(){
   return(
     <div className="Home">
       <h1>Welcome to my flowers shop</h1>
-      <button> We will deliver flowers according to your wishes! 
+      {/* <button> We will deliver flowers according to your wishes! 
         <br></br>We are glad to welcome
-        you in our online flower shop "Million petals" </button>
-        <img src="https://avatars.mds.yandex.net/i?id=c2052abe8ac4753c79619dc160f7b29b944fe170-8159483-images-thumbs&n=13" alt="" />
+        you in our online flower shop "Million petals" </button> */}
+        <img src="" alt="" />
 
     </div>
   )
