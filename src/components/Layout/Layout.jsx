acdc0e2e5cdefa2639a93ac148892d1c34c081a2
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-15 00:25:28 +0600

    swapped div

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -18,18 +18,23 @@ export default function Layout(props) {
   return (
     <div className="Layout">
       <header>
-        <div className="menu">
-          <Nav />
-        </div>
-
         <div className="head_bot">
           <Logo />
+          <div className="search">
+            <input type="text" />
+            <button type="button">
+              <img src="https://i.pinimg.com/564x/11/09/92/110992cd4635d5643ac7a688d68466f4.jpg" alt="" />
+            </button>
+          </div>
           <div className="head_auth">
             <CartLink />
             <Auth />
             <NavToggle callback={toggleDrawer}/>
             <Drawer open={drawerOpen}toggle={toggleDrawer}/>
           </div>
+        </div>
+        <div className="menu">
+          <Nav />
         </div>
       </header>
       <aside>
