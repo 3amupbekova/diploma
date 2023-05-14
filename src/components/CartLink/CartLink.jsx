6f8cab56a16a15f4e537534256adcab432544488
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-15 00:21:26 +0600

    Changed img for CartLink

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -12,7 +12,7 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <Link to="/cart">
-        <img src="https://i.pinimg.com/564x/9a/0f/97/9a0f97efb79c5c2ca85a6c5e0e7f9b28.jpg" alt="" />({total})
+        <img src="https://i.pinimg.com/564x/14/64/53/146453abcd0d77e10444895668151bd0.jpg" alt="" />({total})
       </Link>
     </div>
   );
