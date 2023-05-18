3af322d1084739347fa3044a2f81b0c90c053cbf
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-18 13:53:49 +0600

    Changed logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -7,8 +7,8 @@ export default function Logo() {
   return (
     <div className="Logo">
       <NavLink to="/">
-       <img src="https://i.pinimg.com/564x/8e/b7/fe/8eb7fe34f14d7c361e8339ea2e409e8b.jpg" />
-
+      
+<a href="#" class ="logo"> Flowers <span>.</span></a>
       
         
       </NavLink>
