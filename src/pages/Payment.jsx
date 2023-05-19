1a2a623722e960b86b2c8a1ae842743f6ae5d23b
3amupbekova <elana.zamirbekovaa@gmail.com>
2023-05-19 13:11:11 +0600

    Added payment

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -2,14 +2,21 @@ export default function Payment() {
   return (
     <div className="Payment">
       <h1>Payment</h1>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1067/1067382.png"
+        alt=""
+      />
+      <img
+        src="https://icon-library.com/images/payment-icon-png/payment-icon-png-26.jpg"
+        alt=""
+      />
+
       <p>
         We try to make the ordering process as fast, comfortable and safe as
         possible. In the Million Petals online store, you can choose the most
         convenient way to pay for your order.
       </p>
       <h2>Cash to the courier or by bank details</h2>
-      
-      
     </div>
   );
 }
