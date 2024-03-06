7b31f99ac2fd21d3c5ecba77fa0d2994d11b3740
3amupbekova <elana.zamirbekovaa@gmail.com>
2024-03-06 21:45:52 +0600

    ddd

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -8,7 +8,7 @@ export default function Logo() {
       <NavLink to="/">
         <a href="#" class="logo">
         
-       <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kp.ru%2Fwoman%2Fmoda%2Fluchshie-yuvelirnye-brendy%2F&psig=AOvVaw30DIdGsBmMpghRAp9j5I5t&ust=1709825838149000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCx2N_734QDFQAAAAAdAAAAABAK" alt="logo1" />
+          Jewerly <span>.</span>
         </a>
       </NavLink>
     </div>
