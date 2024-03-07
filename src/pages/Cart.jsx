35a65dcb02068a929533c0daaa9b8651062a7cd8
3amupbekova <elana.zamirbekovaa@gmail.com>
2024-03-07 14:26:39 +0600

    исправила

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import OrderForm from "../components/OrderForm/OrderForm";
 export default function Cart() {
   return (
     <div className="Cart">
-      <h1>Cart</h1>
+      <h1>Карточка товара</h1>
       <CartList />
       <OrderForm />
     </div>
