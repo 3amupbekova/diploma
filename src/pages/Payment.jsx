4b97b35c1bfc7283a717401a788d58fde3ac1734
3amupbekova <elana.zamirbekovaa@gmail.com>
2024-03-07 15:03:43 +0600

    поменяла

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -6,10 +6,7 @@ export default function Payment() {
         src="https://asbestadm.ru/media/project_mo_729/7a/1a/81/e7/1e/aa/acffe5eb80631d5c547f6dcd05fbe6ed.jpg"
         alt=""
       />
-      <img
-        src="https://vesti.kg/media/k2/items/cache/5937850ce4cc1e863d27aadaf775ef4e_XL.jpg?t=20221114_112249"
-        alt=""
-      />
+      
 
       <p>
       Мы создали для вас удобную оплату. Вы можете оплатить свой заказ сидя дома через онлайн платёж.
