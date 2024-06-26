ed29450b94f0d234071ad4905209f8dbc96faf96
3amupbekova <elana.zamirbekovaa@gmail.com>
2024-06-26 23:05:43 +0600

    aaaaa

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,11 +6,10 @@ export default function Logo() {
   return (
     <div className="Logo">
       <NavLink to="/">
-        {/* <a href="#" class="logo">
+        <a href="#" class="logo">
         
-          Jewerly <span>.</span>
-        </a> */}
-        <img src="https://st2.depositphotos.com/24028908/44475/v/450/depositphotos_444751022-stock-illustration-line-art-diamond-jewelry-logo.jpg" alt="" />
+          Jewerly  <span>.</span>
+        </a>
       </NavLink>
     </div>
   );
